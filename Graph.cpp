d633ef66bb243dd02612c935b40c110b881ca77e
halelavigezer <avigezerhalel@gmail.com>
2025-04-08 10:54:54 +0300

    Upload Test

---
+++ Graph.cpp
@@ -0,0 +1,8 @@
+//
+// Created by halel on 4/8/25.
+//
+
+#include "Graph.h"
+
+namespace graph {
+} // graph
---
+++ Graph.h
@@ -0,0 +1,20 @@
+//
+// Created by halel on 4/8/25.
+//
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+namespace graph {
+
+class Graph {
+private:
+    int numVertices;
+public:
+    Graph(int numVertices);
+
+};
+
+} // graph
+
+#endif //GRAPH_H
